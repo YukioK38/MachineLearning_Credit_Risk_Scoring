3fb272c96579e095c99dbc304b613170be246613
YukioK38 <fabioyukiokitsuwa@gmail.com>
2026-06-30 14:17:02 -0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/raw/cs-training.csv")
-
-print(df.shape)        # quantas linhas e colunas
-print(df.dtypes)       # tipos de cada coluna
-print(df.isnull().sum()) # quantos valores ausentes por coluna
-print(df.head())       # primeiras linhas
